8d64322da463db546feb4ee5d050eca488986c09
rsp <36844685+ruishaopu561@users.noreply.github.com>
2019-06-22 23:22:29 +0800

    add gateway

---
+++ se418gateway/src/main/java/exort/se418gateway/Se418gatewayApplication.java
@@ -0,0 +1,28 @@
+package exort.se418gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class Se418gatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Se418gatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        StripPrefixGatewayFilterFactory.Config config = new StripPrefixGatewayFilterFactory.Config();
+        config.setParts(1);
+        return builder.routes()
+                .route("host_route", r -> r.path("/a/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8050"))
+                .route("host_route", r -> r.path("/b/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8040"))
+                .build();
+    }
+}
---
+++ se418gateway/src/test/java/exort/se418gateway/Se418gatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package exort.se418gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Se418gatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
