1c2a5893b43dfc549e9d6d9a655f09bd682d1bb5
rsp <36844685+ruishaopu561@users.noreply.github.com>
2019-06-22 22:09:53 +0800

    implement eureka and ribbon

--- src/main/java/exort/se418/controller/HelloController.java
+++ src/main/java/exort/se418/controller/HelloController.java
@@ -1,6 +1,7 @@
 package exort.se418.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,28 +16,36 @@ public class HelloController{
 	@Autowired
 	private LadderService ladderService;
 
+	@Value("${server.port}")
+	String port;
+
 	@GetMapping("/")
 	public String homepage(){
+		System.out.println("print from: "+port);
 		return "Home Page";
 	}
 
 	@GetMapping("/hello/{who}")
 	public String hello(@PathVariable("who") String who){
+		System.out.println("print from: "+port);
 		return "hello "+who;
 	}
 
 	@GetMapping("/sayhello/{who}")
 	public String sayHello(@PathVariable("who") String who){
+		System.out.println("print from: "+port);
 		return "say "+helloService.hello(who);
 	}
 
 	@GetMapping("/ladder/{begin}/{end}")
 	public String generateLadder(@PathVariable("begin") String begin, @PathVariable("end") String end){
+		System.out.println("print from: "+port);
 		return ladderService.generateLadder(begin,end);
 	}
 
 	@GetMapping("/github")
 	public String ladderDogToCat(){
+		System.out.println("print from: "+port);
 		return helloService.rediectToGithubPage();
 	}
 }
