a5aac10fbcfa41d7096910cd014e48619c03a1c6
MrHate <dgyhateyou@sjtu.edu.cn>
2019-06-27 13:35:19 +0800

    gateway

--- se418gateway/src/main/java/exort/se418gateway/Se418gatewayApplication.java
+++ se418gateway/src/main/java/exort/se418gateway/Se418gatewayApplication.java
@@ -17,7 +17,7 @@ public class Se418gatewayApplication {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 			.route("ladder",r -> r.path("/ladder/dog/cat")
-					.uri("http://ladder:8050/"))
+					.uri("http://212.64.27.71:8050/"))
 			.build();
         //StripPrefixGatewayFilterFactory.Config config = new StripPrefixGatewayFilterFactory.Config();
         //config.setParts(1);
